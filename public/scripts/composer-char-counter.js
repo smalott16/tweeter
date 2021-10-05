6157b929a7397d8e0609115ce691ad06a814893d
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-10-05 21:48:38 +0000

    character counter is working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $( "#tweet-text" ).on("input", function() {
+    //on every key stroke, get the length of this text field
+    const tweetLength = $(this).val().length;
+    if(tweetLength > 140) {
+      $('.counter').css('color','red');
+    } else {
+      console.log(tweetLength);
+      $('.counter').css('color','#545149');
+    }
+    $("#counter").val(140-tweetLength)
+  });
+
+});
