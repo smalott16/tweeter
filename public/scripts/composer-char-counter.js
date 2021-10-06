18c25fac01df8ade681d249942ef9b9ac936f8a6
Spencer Malott <spencermalott16@gmail.com>
2021-10-06 21:30:03 +0000

    ajax request to post a new tweet, ajax request to fetch all tweets upon page load

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,10 +6,11 @@ $(document).ready(function() {
     if(tweetLength > 140) {
       $('.counter').css('color','red');
     } else {
-      console.log(tweetLength);
       $('.counter').css('color','#545149');
     }
     $("#counter").val(140-tweetLength)
   });
+
+  
 
 });
